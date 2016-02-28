4c5af1cb4c90fbea66258bca659727500929c74c
Jasper Tomas <jaspertomas@gmail.com>
2016-02-28 14:25:57 +0800

    added android/login

--- apps/frontend/modules/android/actions/actions.class.php
+++ apps/frontend/modules/android/actions/actions.class.php
@@ -17,21 +17,9 @@ class androidActions extends sfActions
   */
   public function executeIndex(sfWebRequest $request)
   {
-    $this->forward('default', 'module');
   }
   public function executeLogin(sfWebRequest $request)
   {
-  //------no credentials necessary-----------
-	//create output data
-	$output=array("success"=>true,"message"=>"Welcome");
-	
-    //create data array to be sent back to android app
-    $this->getResponse()->setHttpHeader('Content-type','application/json');
-    echo json_encode($output);
-
-    die();
-  //---------------------
-  
 	$json = json_decode(file_get_contents('php://input'),true);
   
     $access_token=md5("hello".$json['username']."its".MyDate::today());
@@ -40,21 +28,21 @@ class androidActions extends sfActions
       echo json_encode(array("success"=>false,"error"=>"Access denied"));
       die();
     }
-  
+
     //if user doesn't exist, fail
     $user = Doctrine_Query::create()
-        ->from('User u')
-      	->where('u.username = '.$request->getParameter('username'))
+        ->from('SfGuardUser u')
+      	->where('u.username = "'.$json['username'].'"')
       	->fetchOne();
     if(!$user)
     {
       echo json_encode(array("success"=>false,"error"=>"Invalid username"));
       die();
     }
-    
+
     //validate password
-    $password="hello";
-    if($request->getParameter('password')!=$password)
+    $password=sha1($user->getSalt().$json['password']);
+    if($password!=$user->getPassword())
     {
       echo json_encode(array("success"=>false,"error"=>"Invalid password"));
       die();
--- lib/model/me/MyDate.class.php
+++ lib/model/me/MyDate.class.php
@@ -3,6 +3,6 @@ class MyDate
 {
   public static function today() { 
     $today = getdate(); 
-    return $today['year']."-".$today['mon']."-".$today['mday'];
+    return $today['year']."-".str_pad($today['mon'],2,"0",STR_PAD_LEFT)."-".str_pad($today['mday'],2,"0",STR_PAD_LEFT);
   }
 }
