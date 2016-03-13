bb9be3bae8e6a62892b07b06e33a30b24c893842
Jasper Tomas <jaspertomas@gmail.com>
2016-03-13 21:59:07 +0800

    report works

--- apps/frontend/modules/record/templates/reportSuccess.php
+++ apps/frontend/modules/record/templates/reportSuccess.php
@@ -2,8 +2,6 @@
 <h1>Attendance Report</h1>   
 
 <?php 
-var_dump($startdate);
-var_dump($enddate);
 //show date form
 echo form_tag("record/report");
 $startDateForm = new sfWidgetFormDate();
@@ -15,13 +13,25 @@ echo " to ".$endDateForm->render('enddatesplit',$enddate);
 </form>
 
 
-<textarea cols=80 rows=50>
-    <?php foreach($records as $record){?>
-      <?php echo $record->getId()?>
-    <?php echo "\t"?>
-      <?php echo $record->getDatetime()?>
-      <?php echo $record->getEmployeeName()?>
-  <?php } ?>
-
+<textarea cols=100 rows=50>
+<?php 
+echo "No	DN	UID              	Name           	Status	Action	APB	JobCode	DateTime\n\n";
+foreach($records as $record)
+{
+echo str_pad($record->getId(), 6, "0", STR_PAD_LEFT)."\t";
+echo "0001\t";
+echo "000000000000000000\t";
+echo str_pad($record->getEmployeeName(), 12, " ", STR_PAD_RIGHT)."\t";
+echo "06\t";
+echo "01\t";
+echo "0\t";
+echo "000\t";
+echo str_replace("-","/",$record->getDatetime())."\n\n";
+} ?>
 </textarea>
 
+<!--
+
+000000	0001	000000000000000004	RUEL        	06	01	0	000	2015/11/14 08:56:11
+
+-->
