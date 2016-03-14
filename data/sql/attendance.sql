aa4a3ce72987bf6455b66d952c97fcd7980c1bdb
Jasper Tomas <jaspertomas@gmail.com>
2016-03-14 11:53:21 +0800

    removed dummy data from sql file

--- data/sql/attendance.sql
+++ data/sql/attendance.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 13, 2016 at 03:21 PM
+-- Generation Time: Mar 14, 2016 at 03:51 AM
 -- Server version: 5.5.27
 -- PHP Version: 5.5.30
 
@@ -36,24 +36,6 @@ CREATE TABLE IF NOT EXISTS `employee` (
 --
 -- Dumping data for table `employee`
 --
-
-INSERT INTO `employee` (`id`, `name`) VALUES
-(5, 'CORA'),
-(8, 'DANNY'),
-(15, 'ELMO'),
-(14, 'ERNESTO'),
-(13, 'GILBERT'),
-(1, 'JASPER'),
-(11, 'JENNIFER'),
-(10, 'JOEBERT'),
-(12, 'LUCIO'),
-(6, 'MARCO'),
-(2, 'MEANN'),
-(4, 'PHILIP'),
-(9, 'RUDY'),
-(3, 'RUEL'),
-(7, 'TATA');
-
 -- --------------------------------------------------------
 
 --
@@ -68,17 +50,7 @@ CREATE TABLE IF NOT EXISTS `record` (
   `is_valid` tinyint(4) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `employee_name` (`employee_name`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=17 ;
-
---
--- Dumping data for table `record`
---
-
-INSERT INTO `record` (`id`, `employee_name`, `datetime`, `filename`, `is_valid`) VALUES
-(12, 'JASPER', '2016-03-13 18:31:34', 'JASPER-2016-03-13-18-31-34.jpg', 0),
-(14, 'JASPER', '2016-03-13 18:31:34', '1', 0),
-(15, 'MEANN', '2', '2', 0),
-(16, 'JASPER', '0', '0', 0);
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=21 ;
 
 -- --------------------------------------------------------
 
@@ -93,26 +65,6 @@ CREATE TABLE IF NOT EXISTS `settings` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=23 ;
-
---
--- Dumping data for table `settings`
---
-
-INSERT INTO `settings` (`id`, `name`, `value`) VALUES
-(1, 'default_warehouse_id', '2'),
-(2, 'me_vendor_id', '83'),
-(3, 'account_id_cash_on_hand', '3'),
-(4, 'account_id_petty_cash', '9'),
-(5, 'account_id_payables', '12'),
-(14, 'account_id_cash_at_home', '6'),
-(15, 'account_id_utilitiesexpense', '7'),
-(16, 'account_id_merchandiseexpense', '8'),
-(17, 'account_id_salesincome', '10'),
-(18, 'account_id_receivables', '11'),
-(19, 'account_id_inchecks', '13'),
-(20, 'account_id_outchecks', '14'),
-(21, 'default_sales_person_id', '2'),
-(22, 'default_purchases_person_id', '2');
 
 -- --------------------------------------------------------
 
@@ -254,7 +206,7 @@ CREATE TABLE IF NOT EXISTS `sf_guard_user` (
 --
 
 INSERT INTO `sf_guard_user` (`id`, `first_name`, `last_name`, `email_address`, `username`, `algorithm`, `salt`, `password`, `is_active`, `is_super_admin`, `last_login`, `created_at`, `updated_at`) VALUES
-(1, 'admin', 'admin', 'a@a.com', 'admin', 'sha1', 'cc1f2f6421dde7479bdb328774efd1c7', '14f10109242c4e5c001c827dbfa5541019b72f73', 1, 0, '2016-03-13 23:16:54', '2010-12-13 09:50:28', '2016-03-13 23:16:54'),
+(1, 'admin', 'admin', 'a@a.com', 'admin', 'sha1', 'cc1f2f6421dde7479bdb328774efd1c7', '14f10109242c4e5c001c827dbfa5541019b72f73', 1, 0, '2016-03-14 10:19:00', '2010-12-13 09:50:28', '2016-03-14 10:19:00'),
 (2, 'Lesley', 'Tomas', 'lesley_tomas@yahoo.com', 'lesley_tomas', 'sha1', 'ac35c23cd9f9a9c5c4693330853150d8', '89ec9d48979740c78b8cc16e2a407e3e93d6df86', 1, 1, '2011-06-10 09:33:54', '2010-12-14 08:14:59', '2011-06-10 09:33:54'),
 (3, 'Jason', '', 'user@user.com', 'jason', 'sha1', 'fbca49049a74774f5d5565b135f9a3d7', 'd8122d2b31b645fc811db2cd1557ddde1b293fe7', 1, 0, '2011-01-03 09:54:27', '2010-12-23 05:34:51', '2011-01-03 09:54:27');
 
